687d5ebfba195434c22316b74cccc0a8113df80b
Stefan Rösch <stefan.roesch1985@gmail.com>
2017-06-22 17:26:49 +0200

    Fix route def bug

--- src/routes/collections.rs
+++ src/routes/collections.rs
@@ -18,7 +18,7 @@ fn get_collection<'r>(id: i32) -> Option<Response<'r>> {
 #[post("/collections/new?<new_collection>")]
 fn new_collection<'r>(new_collection: NewCollection) -> Option<Response<'r>> {
     let store_uri = format!(
-        "/images/new?name={}&description={}",
+        "/collections/new?name={}&description={}",
         new_collection.name,
         new_collection.description
     );
@@ -28,7 +28,7 @@ fn new_collection<'r>(new_collection: NewCollection) -> Option<Response<'r>> {
 #[put("/collections/update?<collection>")]
 fn update<'r>(collection: Collection) -> Option<Response<'r>> {
     let store_uri = format!(
-        "/images/update?id={}name={}&description={}",
+        "/collections/update?id={}name={}&description={}",
         collection.id,
         collection.name,
         collection.description
